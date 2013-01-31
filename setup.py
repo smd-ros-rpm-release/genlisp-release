04c58a27c0997b9d2c5c6d777d77596a2049f69c
Dirk Thomas <dthomas@willowgarage.com>
2013-01-30 17:46:38 -0800

    Rebase from 'upstream' @ version '0.3.3'

--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
+import sys
+from xml.etree.ElementTree import ElementTree
 
-d = generate_distutils_setup(
-    packages=['genlisp'],
-    package_dir={'': 'src'},
-    requires=['genmsg']
-)
+try:
+    root = ElementTree(None, 'stack.xml')
+    version = root.findtext('version')
+except Exception, e:
+    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+    sys.exit(-1)
 
-setup(**d)
+sys.path.insert(0, 'src')
+
+setup(name = 'genlisp',
+      version = version,
+      packages = ['genlisp'],
+      package_dir = {'': 'src'},
+      install_requires = ['genmsg'],
+      scripts = ['scripts/gen_lisp.py'],
+      author = "Bhaskara Marthi",
+      author_email = "bhaskara@willowgarage.com",
+      url = "http://www.ros.org/wiki/genlisp",
+      download_url = "http://pr.willowgarage.com/downloads/genlisp/",
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS msg/srv Lisp generation",
+      long_description = """\
+Library and scripts for generating ROS message data structures in LISP.
+""",
+      license = "BSD"
+      )
